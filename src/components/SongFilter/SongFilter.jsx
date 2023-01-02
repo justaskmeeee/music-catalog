ff48b0fe19741376c6d56da8632b9594c0c20617
justaskmeeee <102696559+justaskmeeee@users.noreply.github.com>
2023-01-02 08:59:27 +0300

    Add search params to URL and new styles

--- src/components/SongFilter/SongFilter.jsx
+++ src/components/SongFilter/SongFilter.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { changeFilterValue } from "store/slices/songSlice";
+import { useSearchParams } from "react-router-dom";
 import s from './SongFilter.module.scss';
 
 const SongFilter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const dispatch = useDispatch();
-  
+
   const handleSongFilterValue = (event) => {
     const { value } = event.target;
-    dispatch(changeFilterValue(value));    
+    dispatch(changeFilterValue(value));
+    setSearchParams({search: value});
+  }
+
+  const removeSongFilterParam = () => {
+    const paramQuery = searchParams.get('search');
+    if (paramQuery === '') {
+      searchParams.delete('search');
+      setSearchParams(searchParams);
+    }
   }
 
   return (
     <div className={s.filter}>
-      <span>Найти:</span>
-      <input onChange={handleSongFilterValue} />
+      <input
+        className={s.searchField}
+        onChange={handleSongFilterValue}
+        onBlur={removeSongFilterParam}
+        placeholder="Найти песню"
+      />
     </div>
   );
 }
