856abaa9f7a28cb70cad12a6bc61c3126b5e3fdf
justaskmeeee <102696559+justaskmeeee@users.noreply.github.com>
2023-01-19 04:30:45 +0300

    Add dispatch to fetch catalog items

--- src/components/MusicCatalogApp/MusicCatalogApp.jsx
+++ src/components/MusicCatalogApp/MusicCatalogApp.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Content from "../Content";
 import Header from '../Header';
+import { useEffect } from 'react';
+import { getSongsFetch } from 'store/slices/songSlice';
+import { useDispatch } from 'react-redux';
+import s from './MusicCatalogApp.module.scss';
 
 const MusicCatalogApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSongsFetch());
+  }, [dispatch]);
+
   return (
-    <>
+    <div className={s.container}>
       <Header />
       <Content />
-    </>
+    </div>
   )
 }
 
